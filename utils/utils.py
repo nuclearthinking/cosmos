879385502b638ac2f6167f65eedaf465dba297b2
Vladislav Radchenko <v.radchenko@cian.ru>
2017-10-31 20:03:59 +0300

    cosmos-1 Добавить рандомизацию при публикации + изменил округление времени публикации + исправил конфликт вызова бота + публикуется рандомная фотография из списка публикации

--- utils/utils.py
+++ utils/utils.py
@@ -2,8 +2,7 @@ import datetime
 
 
 def _round_publication_date(date: datetime.datetime):
-    minutes = date.minute
-    if minutes > 30:
-        return date + datetime.timedelta(minutes=minutes + (60 - minutes))
-    if minutes < 30:
-        return date + datetime.timedelta(minutes=minutes + (30 - minutes))
+    if date.minute > 30:
+        return date + datetime.timedelta(minutes=60 - date.minute) - datetime.timedelta(seconds=date.second)
+    if date.minute < 30:
+        return date + datetime.timedelta(minutes=30 - date.minute) - datetime.timedelta(seconds=date.second)
